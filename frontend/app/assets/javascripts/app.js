d169c384f219f6b7579d845b1f80471c0d7ffe0e
Juan Marin Otero <juan.marin.otero@gmail.com>
2014-08-12 19:36:56 -0400

    Hook up angular services asset

--- frontend/app/assets/javascripts/app.js
+++ frontend/app/assets/javascripts/app.js
@@ -1,9 +1,9 @@
 //define(['angular', 'home', 'dashboard', 'services'], function(angular) {
-  define(['angular'], function(angular) {
+  define(['angular', 'services'], function(angular) {
   'use strict';
 
   // We must already declare most dependencies here (except for common), or the submodules' routes
   // will not be resolved
-	return angular.module('app', []);
+	return angular.module('app', ['app.services']);
   //return angular.module('app', ['yourprefix.home','yourprefix.dashboard', 'yourprefix.services']);
 });
